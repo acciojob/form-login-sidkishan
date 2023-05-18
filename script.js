e0d8fcb269c747897ad93e3f812eaf2b106c2475
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-18 23:10:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getFormvalue() {
     //Write your code here
-var str = document.getElementsByTagName("input");
+var str = document.getElementsByTagName("input").innerText;
 	document.alert(str[0] + " " + str[1]);
 }
